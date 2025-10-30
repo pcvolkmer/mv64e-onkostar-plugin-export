8e59d860d40d6ad2079c203525d11168e3fc48b5
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-30 16:24:44 +0100

    chore: add spotless

--- src/main/java/dev/pcvolkmer/onco/dnpmexport/ExportAnalyzerConfig.java
+++ src/main/java/dev/pcvolkmer/onco/dnpmexport/ExportAnalyzerConfig.java
@@ -1,24 +1,22 @@
 package dev.pcvolkmer.onco.dnpmexport;
 
 import dev.pcvolkmer.onco.datamapper.mapper.MtbDataMapper;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class ExportAnalyzerConfig {
 
-    @Bean
-    public MtbDataMapper mtbDataMapper(final DataSource dataSource) {
-        // Reuse default Onkostar DataSource for MtbDataMapper
-        return MtbDataMapper.create(dataSource);
-    }
+  @Bean
+  public MtbDataMapper mtbDataMapper(final DataSource dataSource) {
+    // Reuse default Onkostar DataSource for MtbDataMapper
+    return MtbDataMapper.create(dataSource);
+  }
 
-    @Bean(name = "mv64eExportRestTemplate")
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
+  @Bean(name = "mv64eExportRestTemplate")
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
